f14855dd50b431d6d514d78cbec404e12280bebe
Gabriel Foriel-Fusier <forielfusierg@educ.college-montreal.qc.ca>
2023-10-16 13:27:28 -0400

    Corriger

--- player.py
+++ player.py
@@ -8,46 +8,46 @@ class Player:
     self.inventory_weapon = ["Rien"]
     self.inventory_armor = ["Rien"]
     self.name = str(input("Comment vous nommez-vous?\n\nReponse: "))
-    self.race = int(input("A quel peuple appartenez-vous:\n1-Humain\n2-Elfe\n3-Nain\n\nReponse: "))
-    self.p_class = int(input("Quel classe voulez etre:\n1-Chevalier\n2-Sorcier\n\nReponse: "))
+    self.race = int(input("A quel peuple appartenez-vous:\n1-Sans dessein\n2-Religieux\n3-Noble\n\nReponse: "))
+    self.p_class = int(input("Quel classe voulez etre:\n1-Chevalier\n2-Inquisiteur\n\nReponse: "))
     print(self.p_class)
     if self.p_class == 1:
       self.p_class = "Chevalier"
       self.inventory_weapon.append("Épée")
       self.inventory_armor.append("Armure de fer")
     else:
-      self.p_class = "Sorcier"
-      self.inventory_weapon.append("Bâton de sorcier")
-      self.inventory_armor.append("Robe de sorcier")
+      self.p_class = "Inquisiteur"
+      self.inventory_weapon.append("Serment de vérité")
+      self.inventory_armor.append("Habit d'inquisiteur")
       
     if self.race == 1:
-      self.race = "Humain"
-      self.stat_int = 10
+      self.race = "Sans dessein"
+      self.stat_faith= 10
       self.stat_strenght = 10
       self.stat_speed = 2
       self.stat_hp = 100
     elif self.race == 2:
-      self.race = "Elfe"
-      self.stat_int = 15
+      self.race = "Religieux"
+      self.stat_faith = 15
       self.stat_strenght = 8
-      self.stat_speed = 4
+      self.stat_speed = 2
       self.stat_hp = 80
     else:
-      self.race = "Nain"
-      self.stat_int = 8
+      self.race = "Noble"
+      self.stat_faith = 8
       self.stat_strenght = 15
-      self.stat_speed = 1
+      self.stat_speed = 2
       self.stat_hp = 120
-    add_int = dice.lancer(1,10)
+    add_faith = dice.lancer(1,10)
     add_strenght = dice.lancer(1,10)
     add_hp = dice.lancer(1,20)
-    self.stat_int += add_int
+    self.stat_faith += add_faith
     self.stat_strenght += add_strenght
     self.stat_hp += add_hp
     self.stat_speed += dice.lancer(1,10)
     Weapon("str",1,0,0)
     Armor(0,0,0)
-    print("Vous avez:\n" + str(self.stat_int) + " point d'intelligence\n" + 
+    print("Vous avez:\n" + str(self.stat_faith) + " point de foi\n" + 
       str(self.stat_strenght) + " point de force\n" + 
       str(self.stat_hp) + " point de vie\n" + 
       str(self.stat_speed) + " de vitesse de deplacement")
@@ -67,12 +67,12 @@ class Player:
           elif self.use_armor == "Armure de fer":
             Armor(10,0,5)
             self.armor = Armor(10,0,5)
-          elif self.use_armor == "Robe de sorcier":
+          elif self.use_armor == "Habit d'inquisiteur":
             Armor(2,10,0)
             self.armor = Armor(2,10,0)
           print("Vous utilisez:\n" + str(self.use_armor)
             + "\nVotre armure a: " +
-                str(self.armor.str_protection) + " de protection physique, " + str(self.armor.int_protection) + " de protection magique et " + str(self.armor.weight) + " de poids")
+                str(self.armor.str_protection) + " de protection physique, " + str(self.armor.faith_protection) + " de protection magique et " + str(self.armor.weight) + " de poids")
         elif choix == 2:
           print("Vous avez:\n" + str(self.inventory_weapon))
           choix = int(input("Quelle arme voulez vous prendre?\n\nReponse: "))
@@ -86,9 +86,9 @@ class Player:
           elif self.use_weapon == "Épée":
             Weapon("str",5,0,self.stat_strenght)
             self.weapon = Weapon("str",5,0,self.stat_strenght)
-          elif self.use_weapon == "Bâton de sorcier":
+          elif self.use_weapon == "Serment de vérité":
             Weapon("int",0,5,0)
-            self.weapon = Weapon("int",0,5,0)
+            self.weapon = Weapon("faith",0,5,0)
           print("Vous utilisez:\n" + str(self.use_weapon)
             + "\nVotre arme fais: " + str(self.weapon.damage) + " points de dégâts")
         elif choix == 3:
--- weapon.py
+++ weapon.py
@@ -1,10 +1,10 @@
 class Weapon:
-  def __init__(self,type,str_damage,int_damage, player_strenght):
+  def __init__(self,type,str_damage,faith_damage, player_strenght):
     self.str_damage = str_damage
-    self.int_damage = int_damage 
+    self.faith_damage = faith_damage 
     self.player_strenght = player_strenght
     self.type = type
-    if self.type == "int":
-      self.damage = self.int_damage
+    if self.type == "faith":
+      self.damage = self.faith_damage
     else:
       self.damage = self.str_damage + self.player_strenght
