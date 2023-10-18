c8e23f158efe7167fafdd9ca25d87b834fc56759
BarySax <144921224+BarySax@users.noreply.github.com>
2023-10-18 09:35:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
 from dice import Dice
+fromfrom dice import Dice
 from player import Player
-from enemy import Enemy
 
 #initialization des classe
-enemy = Enemy()
 
 dice = Dice()
 player = Player()
 dice.lancer(2, 6)
-
-enemy.choose()
-
-dice.lancer(2, 6)
+def level_up():
+  if player.p_class == "Chevalier":
+    choix = int(input("Devant vous ce trouve la statue de Krilum, prophete de la guerre\nQue voulez vous faire:\n1-Vous baissez et honorez votre serment\n2-Partir\n\nReponse:"))
+    if choix == 1:
+      print("Vous fermez les yeux et honorez votre serment")
+      while player.xp >= 0:
+        if player.xp - 100 >= 0:
+          choix = int(input("Vous avez " + str(player.xp) + " point de determination\nQue voulez vous faire:\n1-Augmenter votre foi\n2-Augmenter votre force\n3-Augmenter vos point de vie\n4-Vous lever\n\nReponse: "))
+          if choix == 1:
+            player.stat_faith += 5
+            print("Votre lien avec l'illuminé a augmenter, elle est de " + str(player.stat_faith))
+            player.xp -= 100
+          elif choix == 2:
+            player.stat_strenght += 5
+            print("Votre forcde a augmenter, elle est de " + str(player.stat_strenght))
+            player.xp -= 100
+          elif choix == 3:
+            player.max_hp += 5
+            print("Votre santé maximal a augmenter, elle est de " + str(player.max_hp))
+            player.xp -= 100
+          elif choix == 4:
+            break
+        else:
+          print("Vous navez pas assez")
+          break
+  elif player.p_class == "Inquisiteur":
+    choix = int(input("Devant vous ce trouve la statue de l'eveillememt, dieu incontestable\nQue voulez vous faire:\n1-Vous baissez et suppliez votre divinite\n2-Partir\n\nReponse:"))
+    if choix == 1:
+      print("Vous fermez les yeux et honorez votre serment")
+      while player.xp >= 0:
+        if player.xp - 100 >= 0:
+          choix = int(input("Vous avez " + str(player.xp) + " point de determination\nQue voulez vous faire:\n1-Augmenter votre foi\n2-Augmenter votre force\n3-Augmenter vos point de vie\n4-Vous lever\n\nReponse: "))
+          if choix == 1:
+            player.stat_faith += 5
+            print("Votre lien avec l'illuminé a augmenter, elle est de " + str(player.stat_faith))
+            player.xp -= 100
+          elif choix == 2:
+            player.stat_strenght += 5
+            print("Votre forcde a augmenter, elle est de " + str(player.stat_strenght))
+            player.xp -= 100
+          elif choix == 3:
+            player.max_hp += 5
+            print("Votre santé maximal a augmenter, elle est de " + str(player.max_hp))
+            player.xp -= 100
+          elif choix == 4:
+            break
+        else:
+          print("Vous navez pas assez")
+          break
+level_up()
