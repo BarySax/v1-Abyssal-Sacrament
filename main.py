7f75499e18f9b0eed97606c4eac2300faeba0ddb
Gabriel Foriel-Fusier <forielfusierg@educ.college-montreal.qc.ca>
2023-11-14 15:36:37 -0500

    dawdW

--- main.py
+++ main.py
@@ -10,7 +10,16 @@ player = Player()
 dice.lancer(2, 6)
 fightManager = FightManager()
 
-
+def main_menu():
+  #aficher le debut de lhistoir
+    print("   db    8888Yb Yb  dP .dPY8 .dPY8      db    88         .dP-Y8    db     dP--b8 88--Yb    db    8b    d8 888888 88b 88 888888 ")
+    print("  dPYb   88__dP  YbdP   Ybo.   Ybo.    dPYb   88          Ybo.    dPYb   dP      88__dP   dPYb   88b  d88 88__   88Yb88   88   ")
+    print(" dP__Yb  88  Yb   8P   o. Y8b o. Y8b  dP__Yb  88  .o     o. Y8b  dP__Yb  Yb      88 Yb   dP__Yb  88YbdP88 88     88 Y88   88   ")
+    print("dP    Yb 88oodP  dP    8bodP' 8bodP' dP    Yb 88ood8     8bodP  dP    Yb  YboodP 88  Yb dP    Yb 88 YY 88 888888 88  Y8   88   \n\n\n")
+    while True:
+      start_game = int(input("1-Commencer   2-Charger\n3-Quitter\n\nReponse: "))
+      if start_game == 1:
+        break
 def level_up():
   choix = int(input("Devant vous ce trouve la statue de l'eveillememt, dieu incontestable\nQue voulez vous faire:\n1-Vous baissez et suppliez votre divinite\n2-Partir\n\nReponse:"))
   if choix == 1:
